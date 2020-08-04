deed07ddc5128bf2357b999f5f22b1c64340c6b4
HirokiItani <itanihiroki@gmail.com>
2020-08-04 11:33:00 +0900

    add controller

---
+++ mock/nestjs/src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<User> {
+    return this.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.usersService.delete(id);
+  }
+}
