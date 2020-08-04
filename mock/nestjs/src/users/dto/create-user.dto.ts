a14294622c3eedaa5ccf32a5f36f2620b457d3d9
HirokiItani <itanihiroki@gmail.com>
2020-08-04 12:23:59 +0900

    DTOにValidatorを追加

--- mock/nestjs/src/users/dto/create-user.dto.ts
+++ mock/nestjs/src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, MaxLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MaxLength(255)
   firstName: string;
+
+  @IsNotEmpty()
+  @MaxLength(255)
   lastName: string;
 }
