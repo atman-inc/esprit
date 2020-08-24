ab442cf8cda50702e5a62260a1363ef2ef713b4e
HirokiItani <itanihiroki@gmail.com>
2020-08-24 14:47:04 +0900

    grapqlcodegenを使ってResolverの型を生成する

--- mock/graphql/index.ts
+++ mock/graphql/index.ts
@@ -1,27 +1,31 @@
 import { ApolloServer, gql } from 'apollo-server'
 import { typeDefs } from './lib/schema/typeDefs'
+import { Resolvers, QueryResolvers, User } from './lib/generated/graphql'
 
-const books = [
+const USERS: User[] = [
     {
-      id: 1,
+      id: '1',
       name: 'Harry Potter and the Chamber of Secrets',
       age: 12,
       isActive: true
     },
     {
-      id: 2,
+      id: '2',
       name: 'Jurassic Park',
       age: 20,
       isActive: false
     },
   ];
 
-const resolvers = {
-  Query: {
-    users: () => books,
-  },
+const queryResolver: QueryResolvers = {
+  users: (): User[] => USERS,
+  user: (_, args): User => {
+    return USERS.find(u => u.id === args.id)!
+  }
+}
+const resolvers: Resolvers = {
+  Query: queryResolver,
 };
-
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
