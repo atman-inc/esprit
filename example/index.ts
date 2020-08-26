d2bfd94244ee688bf2a641a24a176e7ca3fcb547
HirokiItani <itanihiroki@gmail.com>
2020-08-26 10:34:17 +0900

    add fastify

---
+++ example/index.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify({
+    logger: true
+})
+
+app.get('/', (req, reply) => {
+    reply.send({ hello: 'world' })
+})
+
+app.listen(3000, "0.0.0.0")
