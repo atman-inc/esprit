96ba9786435fec7255ab78ae8d688b5080b7cc8e
HirokiItani <itanihiroki@gmail.com>
2020-08-31 11:21:59 +0900

    add ormconfig.ts

---
+++ example/lib/infrastructure/orm/ormconfig.ts
@@ -0,0 +1,25 @@
+import config from "config";
+
+interface DBConfig {
+  host: string;
+  name: string;
+  user: string;
+  password: string;
+}
+
+const dbConfig = config.get<DBConfig>("database");
+
+module.exports = {
+  type: "postgres",
+  host: dbConfig.host,
+  port: 5432,
+  username: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.name,
+  entities: ["entities/*.ts"],
+  migrationsTableName: "migration_table",
+  migrations: ["migrations/*.ts"],
+  cli: {
+    migrationsDir: "migrations",
+  },
+};
