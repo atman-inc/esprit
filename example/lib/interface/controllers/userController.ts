05a026a7ddd4ae57c178f71f6f971aa466caa1bd
HirokiItani <itanihiroki@gmail.com>
2020-09-07 17:24:43 +0900

    add create action to user controller

--- example/lib/interface/controllers/userController.ts
+++ example/lib/interface/controllers/userController.ts
@@ -1,16 +1,26 @@
 import { injectable, inject } from "tsyringe";
 import { UserGetListUsecase } from "../../application/usecases/user/list/userGetListUsecase";
+import { UserCreateInputData } from "../../application/usecases/user/create/userCreateInputData";
+import { UserCreateUsecase } from "../../application/usecases/user/create/userCreateUsecase";
 
 @injectable()
 export class UserController {
   constructor(
     @inject("UserGetListUsecase")
-    private readonly getListUsecase: UserGetListUsecase
+    private readonly getListUsecase: UserGetListUsecase,
+    @inject("UserCreateUsecase")
+    private readonly createUsecase: UserCreateUsecase
   ) {}
 
   async index() {
     const users = await this.getListUsecase.handle();
 
     return users;
+  }
+
+  async create(name: string, email: string, password: string, birthday: Date) {
+    const inputData = new UserCreateInputData(name, email, password, birthday);
+
+    return await this.createUsecase.handle(inputData);
   }
 }
--- example/spec/interface/controllers/userController.spec.ts
+++ example/spec/interface/controllers/userController.spec.ts
@@ -1,5 +1,6 @@
 import { UserController } from "../../../lib/interface/controllers/userController";
 import { User } from "../../../lib/domain/entiies/user";
+import { UserCredential } from "../../../lib/domain/values/userCredential";
 
 describe("#index", () => {
   // const mockUsecase = {
@@ -17,4 +18,26 @@ describe("#index", () => {
   // it("return all users", async () => {
   //   expect(await subject()).toHaveLength(2);
   // });
+});
+
+describe("#create", () => {
+  const mockUsecase = {
+    handle: jest.fn(),
+  };
+  mockUsecase.handle.mockReturnValue(
+    new UserCredential(
+      new User(1, "taro", "test@example.com", new Date("1990-01-01"))
+    )
+  );
+  const controller = new UserController(mockUsecase, mockUsecase);
+  const name = "taro";
+  const email = "test@example.com";
+  const password = "password";
+  const birtyday = new Date("1990-01-01");
+
+  it("return user credential", async () => {
+    const result = await controller.create(name, email, password, birtyday);
+
+    expect(result.token).toBe("token1");
+  });
 });
