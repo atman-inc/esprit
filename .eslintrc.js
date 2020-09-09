57f8886d8480c9c457f2608c86901187fbe98dee
HirokiItani <itanihiroki@gmail.com>
2020-09-09 17:17:55 +0900

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
