52a69a53473fb05d9591c8f18b5733fc737c376c
HirokiItani <itanihiroki@gmail.com>
2020-09-14 15:20:36 +0900

    設定ファイル系のテンプレートを追加する

--- packages/create-project/saofile.ts
+++ packages/create-project/saofile.ts
@@ -1,4 +1,5 @@
 import { GeneratorConfig } from "sao";
+import validate from "validate-npm-package-name";
 
 const generator: GeneratorConfig = {
   prompts() {
@@ -18,6 +19,36 @@ const generator: GeneratorConfig = {
           { name: "gRPC", value: "grpc" },
           { name: "GraphQL", value: "graphql" },
         ],
+      },
+    ];
+  },
+  actions() {
+    const validation = validate(this.answers.name);
+    validation.warnings &&
+      validation.warnings.forEach((warn) => {
+        console.warn("Warning:", warn);
+      });
+    validation.errors &&
+      validation.errors.forEach((err) => {
+        console.error("Error:", err);
+      });
+    validation.errors && validation.errors.length && process.exit(1);
+
+    return [
+      {
+        type: "add",
+        files: "**",
+      },
+      {
+        type: "modify",
+        files: "package.json",
+        handler: (data) => {
+          switch (this.answers.model) {
+            case "REST":
+              data["dependencies"]["fastify"] = "^3.3.0";
+          }
+          return data;
+        },
       },
     ];
   },
