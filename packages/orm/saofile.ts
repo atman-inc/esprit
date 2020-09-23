9af66878df0cc07969a84f521f33cb9758b426f1
HirokiItani <itanihiroki@gmail.com>
2020-09-23 18:49:19 +0900

    docker-composeにDBの設定を追加する

--- packages/orm/saofile.ts
+++ packages/orm/saofile.ts
@@ -17,7 +17,7 @@ const generator: GeneratorConfig = {
     ];
   },
   async actions() {
-    // TODO
+    // TODO: Project名をどこから取るか
     const projectName = path.basename(__dirname);
 
     return [
@@ -46,11 +46,47 @@ const generator: GeneratorConfig = {
       },
       {
         type: "modify",
+        files: "docker-compose.yml",
+        handler: (data) => {
+          const yamlData = yaml.load(data) || {};
+
+          const services = yamlData["services"] || {};
+          const appDependsOn = services["app"]["depends_on"] || [];
+
+          appDependsOn.push("db");
+          services["app"]["depends_on"] = appDependsOn;
+
+          services["db"] = {
+            image: "postgres:11-alpine",
+            restart: "always",
+            environment: {
+              POSTGRES_DB: `${projectName}_development`,
+              POSTGRES_USER: "postgres",
+              POSTGRES_PASSWORD: "password",
+            },
+            ports: ["5432:5432"],
+            volumes: ["db_data:/var/lib/postgresql/data"],
+          };
+
+          const volumes = yamlData["volumes"] || {};
+          volumes["db_data"] = { driver: "local" };
+
+          yamlData["services"] = services;
+          yamlData["volumes"] = volumes;
+
+          return yaml.dump(yamlData);
+        },
+      },
+      {
+        type: "modify",
         files: "package.json",
         handler: (data) => {
+          // TODO: Databaseごとに変更する
           data["dependencies"]["pg"] = "^8.3.3";
           data["dependencies"]["typeorm"] = "^0.2.26";
           data["dependencies"]["typeorm-seeding"] = "^1.6.1";
+          data["scripts"]["typeorm"] =
+            "node --require ts-node/register ./node_modules/typeorm/cli.js";
 
           return data;
         },
